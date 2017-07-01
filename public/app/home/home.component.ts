89d44313200521a352f2c1db49ef94ca6b2b9b42
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-07-01 17:33:42 +0100

    Enable communication between server and client

--- public/app/home/home.component.ts
+++ public/app/home/home.component.ts
@@ -1,10 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import * as io from 'socket.io-client';
 
 @Component({
     moduleId: module.id,
-    templateUrl: 'home.template.html'
+    templateUrl: 'home.template.html',
+    styleUrls: [
+        'home.component.css'
+    ]
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+    socket: SocketIOClient.Socket;
+
+    constructor()
+    {
+        this.socket = io.connect();
+    }
+
+    ngOnInit()
+    {
+        this.socket.emit('event1', {
+            msg: 'Client to server, can you hear me server?'
+        });
+        this.socket.on('event2', (data: any) => {
+            console.log(data.msg);
+            this.socket.emit('event3', {
+                msg: 'Yes, its working for me!!'
+            });
+        });
+        this.socket.on('event4', (data: any) => {
+            console.log(data.msg);
+        });
+    }
 
 }
--- server.js
+++ server.js
@@ -10,6 +10,24 @@ app.use(express.static(path.join(__dirname, "public")));
 
 io.on('connection', (socket) => {
     console.log("New connection made");
+
+    // Test messages
+
+    socket.on('event1', (data) => {
+        console.log(data.msg);
+    });
+
+    socket.emit('event2', {
+        msg: 'Server to client, do you read me? Over.'
+    });
+
+    socket.on('event3', (data) => {
+        console.log(data.msg);
+        socket.emit('event4', {
+            msg: 'Loud and clear :)'
+        });
+    });
+
 });
 
 server.listen(port, () => {
